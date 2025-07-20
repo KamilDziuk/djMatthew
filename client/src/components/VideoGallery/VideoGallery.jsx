d19e57fa16698b2759519e515aac5c7ef5dfc72e
kamildziuk <kamildziukwebdeveloper@gmail.com>
2025-07-20 20:27:39 +0200

    refactor(Stress): adding Motion alias
    adding a Motion alias to the motion import and using it in a component in the VideoGallery.jsx file

--- client/src/components/VideoGallery/VideoGallery.jsx
+++ client/src/components/VideoGallery/VideoGallery.jsx
@@ -1,10 +1,10 @@
-import { motion } from "framer-motion";
+import { motion as Motion} from "framer-motion";
 import videoStyles from "./VideoGallery.module.css";
 export default function VideoGallery({ items }) {
   return (
     <>
       {items.map((item, index) => (
-        <motion.video
+        <Motion.video
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           transition={{ duration: 1, ease: "easeInOut" }}
@@ -14,7 +14,7 @@ export default function VideoGallery({ items }) {
           controls
         >
           <source src={item} type="video/mp4"></source>
-        </motion.video>
+        </Motion.video>
       ))}
     </>
   );
