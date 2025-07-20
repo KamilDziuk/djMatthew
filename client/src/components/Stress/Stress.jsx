398a82ec75bfa49b528a530751348bb1f1c25959
kamildziuk <kamildziukwebdeveloper@gmail.com>
2025-07-20 20:22:14 +0200

    refactor(Stress): adding Motion alias
    adding a Motion alias to the motion import and using it in a component in the Stress.jsx file

--- client/src/components/Stress/Stress.jsx
+++ client/src/components/Stress/Stress.jsx
@@ -1,16 +1,16 @@
-import { motion } from "framer-motion";
+import { motion as Motion } from "framer-motion";
 import styleStress from "./Stress.module.css";
 export default function Stress({ children }) {
   return (
     <>
-      <motion.b
+      <Motion.b
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
         transition={{ duration: 2, ease: "easeInOut" }}
         // viewport={{ once: true }}
       >
         {children}
-      </motion.b>
+      </Motion.b>
     </>
   );
 }
