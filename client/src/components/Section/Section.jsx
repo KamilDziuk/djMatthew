4abcedadaf98b70a07e0a9af16e4d51ba2e9399b
kamildziuk <kamildziukwebdeveloper@gmail.com>
2025-07-20 20:27:05 +0200

    refactor(Stress): adding Motion alias
    adding a Motion alias to the motion import and using it in a component in the Section.jsx file

--- client/src/components/Section/Section.jsx
+++ client/src/components/Section/Section.jsx
@@ -1,18 +1,18 @@
-import { motion } from "framer-motion";
+import { motion as Motion} from "framer-motion";
 
 export default function Section({ children, sectionStyle, p }) {
   return (
     <>
       <section className={sectionStyle}>
         {window.location.pathname === "/packages" ? null : (
-          <motion.p
+          <Motion.p
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 1.5, ease: "easeInOut" }}
             // viewport={{ once: true }}
           >
             {p}
-          </motion.p>
+          </Motion.p>
         )}
         {children}
       </section>
